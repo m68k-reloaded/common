1adcc6433b9cf49f0fb7a193ff442dacda6ce623
Marcel Garus <marcel.garus@gmail.com>
2020-03-05 15:11:47 +0100

    put ErrorCollector into its own file

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,23 +1,8 @@
 use crate::Range;
+mod collector;
+pub use collector::ErrorCollector;
 
 pub struct Error {
     pub range: Range,
     pub message: String,
-}
-
-pub struct ErrorCollector {
-    errors: Vec<Error>,
-}
-
-impl ErrorCollector {
-    pub fn push(&mut self, error: Error) {
-        self.errors.push(error);
-    }
-
-    pub fn dump(&self) {
-        for error in &self.errors {
-            // TODO(marcelgarus): print the error
-            println!("Error at byte {}: {}", error.range.start, error.message);
-        }
-    }
 }
