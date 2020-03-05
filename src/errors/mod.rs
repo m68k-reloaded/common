9dba4b444742a6a581f6ddf2e7e957ec7db803c0
Marcel Garus <marcel.garus@gmail.com>
2020-03-05 14:26:42 +0100

    make ErrorCollector a struct for more control about how errors can be collected

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,8 +1,23 @@
 use crate::Range;
 
-pub type ErrorCollector = dyn Fn(Error) -> ();
-
 pub struct Error {
     pub range: Range,
     pub message: String,
+}
+
+pub struct ErrorCollector {
+    errors: Vec<Error>,
+}
+
+impl ErrorCollector {
+    pub fn push(&mut self, error: Error) {
+        self.errors.push(error);
+    }
+
+    pub fn dump(&self) {
+        for error in &self.errors {
+            // TODO(marcelgarus): print the error
+            println!("Error at byte {}: {}", error.range.start, error.message);
+        }
+    }
 }
